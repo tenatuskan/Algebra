c1d7f15e58bcf17691016955f2573f4fa3f2978f
tenatuskan <tenatuskan@gmail.com>
2026-04-28 18:17:42 +0200

    last

---
+++ 8_Studenti/Tena/3_JS/V24_Todo/script.js
@@ -0,0 +1,18 @@
+(function () {
+  function Todo(){
+    function addListItem(){}
+    function checkListItem(){}
+    function removeList Item(){}
+    function showAll(){}
+    function showActive(){}
+    function showCompleted(){}
+    function removeAllCompleted(){}
+    this addListeners = function(){};
+
+  }
+  var todo= new Todo();
+  window.addEventListener("load", function(){
+    console.log("Loaded!")
+  })
+  });
+})();
